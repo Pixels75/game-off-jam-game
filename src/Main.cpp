9c8e1c602a078e07ad3bf63e2a4ae78765cdd3e4
Pixels <164868626+Pixels75@users.noreply.github.com>
2024-11-07 21:46:17 +0300

    Add 3D demo

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,26 @@
 #include "raylib.h"
 
-#include "Object/Object.h"
-
 int main() {
     SetConfigFlags(FLAG_VSYNC_HINT);
     InitWindow(640, 480, "Hello Raylib");
-    Object obj;
+
+    const Model suzanne = LoadModel("../res/test.obj");
+    const Texture2D texture = LoadTexture("../res/checkerboard.png");
+    suzanne.materials[0].maps[MATERIAL_MAP_ALBEDO].texture = texture;
+
+    Camera3D cam;
+    cam.position = Vector3(3, 1, 3);
+    cam.fovy = 60.0f;
+    cam.projection = CAMERA_PERSPECTIVE;
+    cam.target = Vector3(0, 0, 0);
+    cam.up = Vector3(0, 1, 0);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        obj.Draw();
+        BeginMode3D(cam);
+        DrawModel(suzanne, {0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
+        EndMode3D();
         EndDrawing();
     }
 
